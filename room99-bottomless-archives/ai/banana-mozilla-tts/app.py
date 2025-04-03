180b8372a7b7d2871f645d3208129431492f60d2
Astrid Planchon <v3rmine@astriiid.fr>
2025-04-03 14:58:59 +0200

    feat: added archives/ai banana-mozilla-tts

---
+++ room99-bottomless-archives/ai/banana-mozilla-tts/app.py
@@ -0,0 +1,23 @@
+from TTS.api import TTS
+
+# Init is ran on server startup
+# Load your model to GPU as a global variable here using the variable name "model"
+def init():
+    global model
+    model["tts_models/fr/mai/tacotron2-DDC"] = TTS.
+
+# Inference is ran for every server call
+# Reference your preloaded global model variable here.
+def inference(model_inputs:dict) -> dict:
+    global model
+
+    # Parse out your arguments
+    prompt = model_inputs.get('prompt', None)
+    if prompt == None:
+        return {'message': "No prompt provided"}
+    
+    # Run the model
+    result = model(prompt)
+
+    # Return the results as a dictionary
+    return result
