fefec7dad9d0a172629f839d92bbbff2027b701e
Astrid Planchon <v3rmine@astriiid.fr>
2025-04-16 14:45:39 +0200

    feat: added deploy-rs command in just to deploy tailscale

--- room04-sysadmin-bodies/nas-graphiurus/containers/tailscale/flake.nix
+++ room04-sysadmin-bodies/nas-graphiurus/containers/tailscale/flake.nix
@@ -4,23 +4,44 @@
   # For accessing `deploy-rs`'s utility Nix functions
   inputs.deploy-rs.url = "github:serokell/deploy-rs";
 
-  outputs = { self, nixpkgs, deploy-rs }: {
-    nixosConfigurations.tailscale = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs, deploy-rs }:
+    let
       system = "x86_64-linux";
-      modules = [ ./configuration.nix ];
-    };
+      # Unmodified nixpkgs
+      pkgs = import nixpkgs { inherit system; };
+      # nixpkgs with deploy-rs overlay but force the nixpkgs package
+      deployPkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          deploy-rs.overlay
+          (self: super: {
+            deploy-rs = {
+              inherit (pkgs) deploy-rs;
+              lib = super.deploy-rs.lib;
+            };
+          })
+        ];
+      };
+    in {
+      nixosConfigurations.tailscale = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./configuration.nix ];
+      };
 
-    deploy.nodes.tailscale = {
+      deploy.nodes.tailscale = {
         hostname = "tailscale";
         sshUser = "root";
+        remoteBuild = false;
 
         profiles.system = {
           user = "root";
-          path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.tailscale;
+          path = deployPkgs.deploy-rs.lib.activate.nixos
+            self.nixosConfigurations.tailscale;
         };
-    };
+      };
 
-    # This is highly advised, and will prevent many possible mistakes
-    checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
-  };
+      # This is highly advised, and will prevent many possible mistakes
+      checks = builtins.mapAttrs
+        (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
+    };
 }
