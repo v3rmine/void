65d92d58db6d0c4924e175299e7b4a2483d13608
Astrid Planchon <v3rmine@astriiid.fr>
2025-05-19 14:02:24 +0200

    feat: created chip8 emu learning project

--- room01-garden-of-learning/chip8-emulator/README.md
+++ room01-garden-of-learning/chip8-emulator/README.md
@@ -2,3 +2,10 @@
 - https://chip-8.github.io/links/
 - https://tobiasvl.github.io/blog/write-a-chip-8-emulator/
 - https://aquova.net/emudev/chip8/
+
+## Execution Worflow 
+1. Chip8 ROM => Flat IR
+2. Flat IR => AST
+3. AST => Target IR (WASM? LLVM?)
+4. Target IR => Native Code
+5. Execution
--- room01-garden-of-learning/chip8-emulator/src/main.rs
+++ room01-garden-of-learning/chip8-emulator/src/main.rs
@@ -1,3 +1,32 @@
+use bevy::app::ScheduleRunnerPlugin;
+use bevy::prelude::*;
+use bevy_ratatui::{RatatuiContext, RatatuiPlugins};
+
 fn main() {
-    println!("Hello, world!");
+    dioxus_devtools::connect_subsecond();
+    let frame_time = std::time::Duration::from_secs_f32(1. / 60.);
+
+    App::new()
+        .add_plugins((
+            MinimalPlugins.set(ScheduleRunnerPlugin::run_loop(frame_time)),
+            RatatuiPlugins::default(),
+        ))
+        .add_systems(Update, patched)
+        .run();
+}
+
+fn patched(mut context: ResMut<RatatuiContext>) {
+    context.clear().unwrap();
+    dioxus_devtools::subsecond::HotFn::current(draw_system)
+        .call((context,))
+        .ok();
+}
+
+fn draw_system(mut context: ResMut<RatatuiContext>) -> Result {
+    context.draw(|frame| {
+        let text = ratatui::text::Text::raw(format!("hello {}", frame.count()));
+        frame.render_widget(text, frame.area());
+    })?;
+
+    Ok(())
 }
