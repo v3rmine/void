836003c2ca816d2af195014d5e8bf008b9cb06cc
Astrid Planchon <v3rmine@astriiid.fr>
2025-05-20 10:41:41 +0200

    feat: working subsecond with ratatui

--- room01-garden-of-learning/chip8-emulator/src/main.rs
+++ room01-garden-of-learning/chip8-emulator/src/main.rs
@@ -3,7 +3,15 @@ use bevy::prelude::*;
 use bevy_ratatui::{RatatuiContext, RatatuiPlugins};
 
 fn main() {
-    dioxus_devtools::connect_subsecond();
+    // Because connect_subsecond does not work outside of dx serve (which does not work with TUIs)
+    dioxus_devtools::connect_at("ws://127.0.0.1:8080/_dioxus".to_string(), |msg| {
+        if let dioxus_devtools::DevserverMsg::HotReload(hot_reload_msg) = msg {
+            if let Some(jumptable) = hot_reload_msg.jump_table {
+                unsafe { dioxus_devtools::subsecond::apply_patch(jumptable).unwrap() };
+            }
+        }
+    });
+
     let frame_time = std::time::Duration::from_secs_f32(1. / 60.);
 
     App::new()
@@ -24,7 +32,7 @@ fn patched(mut context: ResMut<RatatuiContext>) {
 
 fn draw_system(mut context: ResMut<RatatuiContext>) -> Result {
     context.draw(|frame| {
-        let text = ratatui::text::Text::raw(format!("hello {}", frame.count()));
+        let text = ratatui::text::Text::raw("Hello world!");
         frame.render_widget(text, frame.area());
     })?;
 
