0adc5831475c1417b70d9d41f81895e51fda83c4
v3rmine <v3rmine@astriiid.fr>
2026-03-19 18:52:40 +0100

    feat(room09-chaotic-library): added wander

---
+++ room09-chaotic-library/static/wander/wander.js
@@ -0,0 +1,17 @@
+window.wander = {
+  // Other Wander consoles that visitors can reach from my console.
+  consoles: [
+      'https://astriiid.fr/wander/'
+  ],
+  // Websites and pages I recommend to other wanderers.
+  pages: [
+    // 'https://astriiid.fr/', // Own website
+    'https://tonsky.me/',
+    // 'https://bouvier.cc/', // X-Frame-Options sameorigin
+    // 'https://phd.julien-cpsn.com/', // X-Frame-Options sameorigin
+    'https://jvns.ca/',
+    // 'https://val.packett.cool/', // X-Frame-Options sameorigin
+    // 'https://sebsauvage.net/wiki/', // X-Frame-Options sameorigin
+    // 'https://halimedemf.substack.com/', // X-Frame-Options sameorigin
+  ],
+};
