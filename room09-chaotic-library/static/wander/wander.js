7492acde994099c8707452683c6a63439576fc4f
v3rmine <v3rmine@astriiid.fr>
2026-03-31 17:39:01 +0200

    feat(room09-chaotic-library): updated wander and added filter wild articles in webpage

--- room09-chaotic-library/static/wander/wander.js
+++ room09-chaotic-library/static/wander/wander.js
@@ -1,17 +1,34 @@
 window.wander = {
-  // Other Wander consoles that visitors can reach from my console.
-  consoles: [
-      'https://astriiid.fr/wander/'
-  ],
-  // Websites and pages I recommend to other wanderers.
-  pages: [
-    // 'https://astriiid.fr/', // Own website
-    'https://tonsky.me/',
-    // 'https://bouvier.cc/', // X-Frame-Options sameorigin
-    // 'https://phd.julien-cpsn.com/', // X-Frame-Options sameorigin
-    'https://jvns.ca/',
-    // 'https://val.packett.cool/', // X-Frame-Options sameorigin
-    // 'https://sebsauvage.net/wiki/', // X-Frame-Options sameorigin
-    // 'https://halimedemf.substack.com/', // X-Frame-Options sameorigin
-  ],
+    // Other Wander consoles that visitors can reach from my console.
+    consoles: [
+        'https://astriiid.fr/wander/'
+    ],
+    // Websites and pages I recommend to other wanderers.
+    pages: [
+        // 'https://astriiid.fr/', // Own website
+        'https://tonsky.me/',
+        // 'https://bouvier.cc/', // X-Frame-Options sameorigin
+        // 'https://phd.julien-cpsn.com/', // X-Frame-Options sameorigin
+        'https://jvns.ca/',
+        // 'https://val.packett.cool/', // X-Frame-Options sameorigin
+        // 'https://sebsauvage.net/wiki/', // X-Frame-Options sameorigin
+        // 'https://halimedemf.substack.com/', // X-Frame-Options sameorigin
+        'https://shellsharks.com/',
+        'https://xn--gckvb8fzb.com/',
+        // 'https://blog.avas.space', // X-Frame-Options sameorigin
+        // 'https://chronicles.mad-scientist.club/', // fame-ancestors 'self'
+        'https://sdomi.pl/',
+        'https://purplesyringa.moe/',
+        'https://maia.crimew.gay/',
+        'https://blog.i5-650.systems/',
+        // 'https://brr.fyi/', // X-Frame-Options deny
+        'https://astrid.tech/',
+        'https://zoriya.dev/',
+        // 'https://blog.sunfishcode.online/', // X-Frame-Options sameorigin
+        'https://j3s.sh/',
+        'https://leanrada.com/',
+    ],
+    // Websites and consoles to ignore.  My console will never fetch
+    // consoles or web pages whose URLs match the following patterns.
+    ignore: [],
 };
